2eae06615a569e51a6f1293fa8936e5a5aec767b
Javier Burguete <jburguete@eead.csic.es>
2025-03-22 10:36:04 +0100

    Added SDL3 to MacOS in the generate perl script

--- generate.pl
+++ generate.pl
@@ -1142,27 +1142,27 @@ elsif ($os eq "Darwin")
                     );
     @preinstall = ("@installcask @packagescask");
     @packages = (
-                 "gnu-sed",       "gpatch",
-                 "autoconf",      "automake",
-                 "pkgconf",       "gcc",
-                 "llvm",          "make",
-                 "meson",         "git",
-                 "subversion",    "libxml2",
-                 "gettext",       "glib",
-                 "json-glib",     "sqlite",
-                 "gsl",           "libgtop",
-                 "gtk+3",         "gtk4",
-                 "freeglut",      "glfw",
-                 "sdl2",          "glew",
-                 "glslang",       "mpich",
-                 "font-freefont", "font-open-sans",
-                 "nedit",         "gnu-indent",
-                 "perltidy",      "maxima",
-                 "ddd",           "graphviz",
-                 "texlive",       "latexdiff",
-                 "evince",        "doxygen",
-                 "wget",          "imagemagick",
-                 "mpv"
+                 "gnu-sed",        "gpatch",
+                 "autoconf",       "automake",
+                 "pkgconf",        "gcc",
+                 "llvm",           "make",
+                 "meson",          "git",
+                 "subversion",     "libxml2",
+                 "gettext",        "glib",
+                 "json-glib",      "sqlite",
+                 "gsl",            "libgtop",
+                 "gtk+3",          "gtk4",
+                 "freeglut",       "glfw",
+                 "sdl2",           "sdl3",
+                 "glew",           "glslang",
+                 "mpich",          "font-freefont",
+                 "font-open-sans", "nedit",
+                 "gnu-indent",     "perltidy",
+                 "maxima",         "ddd",
+                 "graphviz",       "texlive",
+                 "latexdiff",      "evince",
+                 "doxygen",        "wget",
+                 "imagemagick",    "mpv"
                 );
 }
 else
